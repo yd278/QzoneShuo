59a500d3c3ccc6aa59d665d7de89c8eba09edbf1
yd278 <yd278@cam.ac.uk>
2017-02-07 21:41:00 +0000

    version 0.2

--- src/Demo.java
+++ src/Demo.java
@@ -3,13 +3,13 @@
  */
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Demo {
 
@@ -35,7 +35,7 @@ public class Demo {
         List<String> result = new ArrayList<>();
         WebDriver driver = new EdgeDriver();
         //打开
-        driver.get("http://user.qzone.qq.com/"+ account + "/311");
+        driver.get("http://qzone.qq.com");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
@@ -52,6 +52,14 @@ public class Demo {
         //登录完成 等页面加载，视网速调时间
 
         try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.get("http://user.qzone.qq.com/"+ account + "/311");
+
+        try {
             Thread.sleep(6000);
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -61,17 +69,24 @@ public class Demo {
         boolean notEnd = true;
         while (notEnd) {
             try {
-                Thread.sleep(2000);
+                Thread.sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            List<WebElement> contents = driver.findElements(By.cssSelector(".content"));
-            List<WebElement> times = driver.findElements(By.cssSelector(".c_tx.c_tx3.goDetail"));
-            for (int i = 0; i < contents.size(); i++) {
-                String s = times.get(i).getText() + " " + contents.get(i).getText().replace("\n", "\\n");
-                result.add(s);
-            }
 
+            List<WebElement> shuos = driver.findElements(By.cssSelector(".feed"));
+            for(WebElement shuo : shuos){
+                String shuoResult = "";
+                String content = shuo.findElement(By.cssSelector(".content")).getText();
+                String time = shuo.findElement(By.cssSelector(".c_tx.c_tx3.goDetail")).getAttribute("title");
+                shuoResult += time + "\n" + content;
+                try{
+                    WebElement pre = shuo.findElement(By.cssSelector(".quote.bor2")).findElement(By.cssSelector(".bd"));
+                    shuoResult += " 【转自：】 " + pre.getText();
+                }catch(NoSuchElementException ignored){
+                }
+                result.add(shuoResult);
+            }
             try{
                 WebElement nextPage = driver.findElement(By.linkText("下一页"));
                 nextPage.click();
